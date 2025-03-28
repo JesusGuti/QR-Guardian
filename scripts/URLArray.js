9014c0033451a6cfa6fc31b37e87e563a81637f9
JesusGuti <jesusgonzales0968@gmail.com>
2025-03-28 11:17:18 -0400

    Error en condicion logica

--- scripts/URLArray.js
+++ scripts/URLArray.js
@@ -1,15 +1,18 @@
 export const URLArray = [
-    'c4munitionoffwithe001x.web.app',
+    'https://sat.gob.mx-subastas.online/honda-crv-2017/',
+]
+
+const otherURLs = [
+
+]
+
+const checkedURLs = [
+    'https://tinyurl.com/f4ceb0okk', // ACORTADA - CAIDO
+    'https://tinyurl.com/fcyt-admision', // ACORTADA - SEGURO
     'http://posgradovirtual.fcyt.umss.edu.bo/', // SEGURO
-    'c4ncelarc0mprasg.vercel.app',
-    'https://tinyurl.com/fcyt-admision', // SEGURO
-    'c4ncelarsucurs4ls.vercel.app', // NO CONTIENE URL
-    'cache.netflix.com.sg1.workers2.xyz',
-    'https://www.redeszone.net/',
-    'https://tinyurl.com/f4ceb0okk', // CAIDO
+    'https://www.redeszone.net/', // SEGURO
     'https://online.secure-service-01.com/ssa1/', // MALICIOSO
     'https://www.trump.tongateway.top', // MALICIOSO
-    'steanmscommnuity.com', // NO CONTIENE URL
     'https://micuenta-actualizacion.com/', // MALICIOSO
     'https://forms.office.com/r/fLKfYvxxni', // No detectado por VirusTotal - QRGuardian SOSPECHOSO
     'https://beneficiosresgate.com/resgate/presell/?name=BENEDITO%2520DAMASCENO%2520DE%2520MIRANDA&document=04249194272&email=ditoenator@gmail.com&telephone=91984703108', // MALICIOSO
@@ -19,5 +22,80 @@ export const URLArray = [
     'https://aumentoupgrade.s3.us-east-2.amazonaws.com/index.html', // Maliciosa
     'http://aventurascol.webcindario.com/', // MALICIOSO
     'https://httpsgggssupplemeentosoficial.shop/ wpm@572a9d70wb1afbfdep8f1e6aabm30a09288/custom/ web-pixel-shopify-custom-pixel@0411/sandbox/modern/', // MALICIOSO
+    'https://c4munitionoffwithe001x.web.app',
+    'https://c4ncelarc0mprasg.vercel.app',
+    'https://c4ncelarsucurs4ls.vercel.app',
+    'https://cache.netflix.com.sg1.workers2.xyz',
+    'https://steanmscommnuity.com',
+    'https://tinyurl.com/bievenidosmuchachos',
+    'https://earnsapien-rewardsreferral.lol',
+    'https://glqlabs.top',
+    'http://suporteacess.net/verificacao/?id=abuse@terra.com.br',
+    'https://sonic-labs.cc',
+    'https://www.nintendo.com/es-es/?srsltid=AfmBOoqqDlT65pc1yMPLBEx8OVaN8nl8e7Z-83JPjTAQA7reDITDWmV2',
+    'http://facebookcontacthelp.com/',
+    'http://101.173.169.186',
+    'https://astro.build/',
+    'https://ameazon-equency.missmichelle.net/apsigninopenid/',
+    'https://nestjs.com/',
+    'https://tinyurl.com/leones-rugiendo',
+    'https://estasfeta.life/',
+    'https://learn.microsoft.com/es-es/windows/wsl/install',
+    'https://interactive-au.com/',
+    'http://bit.ly/4iIZe6F',
+    'https://icpc.global/',
+    'https://swizzinvestmentoption.live/',
+    'https://ezdrive.sqjll.vip/',
+    'https://aappleteamcommunitysupport.pages.dev/',
+    'https://asset-mantra.com',
+    'https://capuchino-scesi.web.app/',
+    'http://tinyurl.com/snapshotmub',
+    'http://7spotify.com',
+    'https://bit.ly/3DVFxt8',
+    'https://e-zpassny.com-usr.vip/',
+    'https://developer.android.com/studio?gad_source=1&gclid=CjwKCAjw7pO_BhAlEiwA4pMQvMHHH0T384-Z2I55Epevd9l9czv6E1Gjn1xAN5UMElK-bo1G-RZzZBoC4eoQAvD_BwE&gclsrc=aw.ds&hl=es-419',
+    'https://sites.google.com/view/lewbau/home',
+    'https://porfolio.dev/',
+    'http://apprlpo.app/login/',
+    'https://shorturl.at/H7M6g',
+    'https://tinyurl.com/suplements-arnold',
+    'https://gemini-login-io.webflow.io',
+    'https://epicpromotions.com/Hetzner/hetzner/web/login.php',
+    'https://trezorstartioh.gitbook.io/us',
+    'https://www.cloudflare.com/es-es/learning/security/what-is-quishing/',
+    'https://bit.ly/4j9mydD',
+    'https://atlus.com/',
+    'https://shorturl.at/A22yb',
+    'http://atmbolivia.com/',
+    'https://2nds28th.b-cdn.net/OIN.html?b=YUBiLmNvbQ==',
+    'https://link-reward.org',
+    'https://store.steampowered.com/games/?l=spanish',
+    'https://helenchalkley578-od3.kleap.co/',
+    'https://shorturl.at/xrAlf',
+    'https://www.scesi.org/',
+    'http://www.ajbellintshares.online',
+    'https://shorturl.at/TNlQm',
+    'https://shorturl.at/KkN3U',
+    'https://shorturl.at/dT9aG',
+    'https://docs.expo.dev/',
+    'https://shorturl.at/8smfN',
+    'https://venzoargentina.com.ar/productos/bicicletas/mountain-bike/',
+    'https://shorturl.at/NeBp6',
+    'https://www.yamabol-srl.com/',
+    'https://shorturl.at/9j0pl',
+    'https://shorturl.at/WLd6R',
+    'https://openboxesclick.tesbw.app',
+    'https://reactnative.dev/',
+    'https://shorturl.at/x3shz',
+    'http://wallet-tonkeeper.com',
+    'https://shorturl.at/fRDz4',
+    'https://shorturl.at/R3oRy',
+    'https://fedex.com-cvxn.xin/',
+    'https://www.typescriptlang.org/',
+    'https://www.python.org/',
+    'https://nodejs.org/en',
+    'https://clockworkcomputerip.blogspot.com/',
+    'https://gchq.github.io/CyberChef/',
+    'http://account.instagramfacebook.click',
+    'https://latam.kaspersky.com/resource-center/definitions/what-is-social-engineering'
 ]
-
