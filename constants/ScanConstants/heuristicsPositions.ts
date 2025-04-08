da1eb961fada0a493c4a0b4d4e3e96d94a17312c
JesusGuti <jesusgonzales0968@gmail.com>
2025-04-08 19:41:37 -0400

    Añadiendo logica para mostrar checkbox de typosquatting

---
+++ constants/ScanConstants/heuristicsPositions.ts
@@ -0,0 +1,5 @@
+export const TLD_POSITION = 0;
+export const DOMAIN_CHECKED_POSITION = 1;
+export const TYPOSQUATTING_SUBDOMAIN_POSITION= 3;
+export const TYPOSQUATTING_DOMAIN_POSITION = 2;
+export const SCANNED_POSIITON = 4;
