4f5c9b9184eece54d99d5b1f8f6507091deebdce
JesusGuti <jesusgonzales0968@gmail.com>
2025-04-15 15:49:30 -0400

    Añadiendo creación de carpeta qr-codes de forma automatica

--- scripts/generateQRCodes.js
+++ scripts/generateQRCodes.js
@@ -1,11 +1,30 @@
 import * as QRCode from "qrcode"
 import { randomUUID } from "node:crypto"
-import { join, dirname } from "node:path"
 import { fileURLToPath } from "node:url"
 import { URLArray } from "./URLArray.js"
+import { 
+    dirname,    
+    join, 
+} from "node:path"
+import { 
+    existsSync,
+    mkdirSync
+} from "node:fs"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+
+const qrCodeDir = join(__dirname, "../qr-codes");
+
+// Verifica si la carpeta qr-codes no existe y la crea si es necesario
+if (!existsSync(qrCodeDir)) {
+  try {
+    mkdirSync(qrCodeDir, { recursive: true }); // { recursive: true } crea carpetas padre si no existen
+    console.log(`Se creó la carpeta: ${qrCodeDir}`);
+  } catch (error) {
+    throw Error("Ocurrió un error al crear la carpeta qr-codes:", error);
+  }
+}
 
 const options = {
     type: "png",
@@ -18,8 +37,8 @@ const options = {
     margin: 2,
 }
 
-async function generateQRCode (URL) {
-    const filePath = join(__dirname, "../qr-codes", `${randomUUID()}.png`)
+function generateQRCode (URL) {
+    const filePath = join(qrCodeDir, `${randomUUID()}.png`)
 
     try {
         QRCode.toFile(filePath, URL, options)
